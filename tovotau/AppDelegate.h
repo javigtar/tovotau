7ce25277280d96918819291112104755ea9fbe77
alumno <alumno@Mac-mini-de-administrador-4.local>
2015-01-21 10:23:08 +0100

    Principio

---
+++ tovotau/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  prueba
+//
+//  Created by alumno on 14/1/15.
+//  Copyright (c) 2015 alumno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
