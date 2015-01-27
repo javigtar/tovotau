91171facabd0e75de2bc288f6cb3f957cca68e00
javi <javi@javi>
2015-01-27 11:59:18 +0100

    Animated Label

--- tovotau/ReproduciendoViewController.h
+++ tovotau/ReproduciendoViewController.h
@@ -10,4 +10,6 @@
 
 @interface ReproduciendoViewController : UIViewController
 
+
+
 @end
--- tovotau/ReproduciendoViewController.m
+++ tovotau/ReproduciendoViewController.m
@@ -10,6 +10,8 @@
 
 @interface ReproduciendoViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *proximasCanciones;
+
 @end
 
 @implementation ReproduciendoViewController
@@ -17,11 +19,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.proximasCanciones.text = @"Cancion 1, Cancion 2, Cancion 3, Cancion 4, Cancion 5, Cancion 6, Cancion 7, Cancion 8, Cancion 9, Cancion 10";
+    
+    [self labelAnimation];
+    [NSTimer scheduledTimerWithTimeInterval:7.0 target:self selector:@selector(labelAnimation)
+                                   userInfo:nil
+                                    repeats:YES];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+-(void)labelAnimation
+{
+    [UIView animateWithDuration:7.0f delay:0.0f options:UIViewAnimationOptionTransitionNone animations:^{
+        self.proximasCanciones.frame = CGRectMake(-320, self.proximasCanciones.frame.origin.y, self.proximasCanciones.frame.size.width, self.proximasCanciones.frame.size.height);
+    } completion:^(BOOL finished)
+     {
+         self.proximasCanciones.frame = CGRectMake(320, self.proximasCanciones.frame.origin.y, self.proximasCanciones.frame.size.width, self.proximasCanciones.frame.size.height);
+     }];
+    
 }
 
 /*
