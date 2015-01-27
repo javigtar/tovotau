8daa5a6c3f5d515f5ac13c3ba41233a54d310a8d
javi <javi@javi>
2015-01-27 11:41:46 +0100

    Modificaciones

---
+++ tovotau/ReproduciendoViewController.h
@@ -0,0 +1,13 @@
+//
+//  ReproduciendoViewController.h
+//  tovotau
+//
+//  Created by alumno on 27/01/15.
+//  Copyright (c) 2015 alumno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReproduciendoViewController : UIViewController
+
+@end
---
+++ tovotau/ReproduciendoViewController.m
@@ -0,0 +1,37 @@
+//
+//  ReproduciendoViewController.m
+//  tovotau
+//
+//  Created by alumno on 27/01/15.
+//  Copyright (c) 2015 alumno. All rights reserved.
+//
+
+#import "ReproduciendoViewController.h"
+
+@interface ReproduciendoViewController ()
+
+@end
+
+@implementation ReproduciendoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
