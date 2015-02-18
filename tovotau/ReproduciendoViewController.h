620858bc5cc8a4d8f30f77a3549d2b9ff34db3fc
alumno <alumno@Mac-mini-de-administrador-8.local>
2015-02-18 08:19:35 +0100

    Top5 implementando...

--- tovotau/ReproduciendoViewController.h
+++ tovotau/ReproduciendoViewController.h
@@ -10,6 +10,8 @@
 
 @interface ReproduciendoViewController : UIViewController
 @property (weak, nonatomic) IBOutlet UIImageView *banner;
+@property (weak, nonatomic) IBOutlet UILabel *labelCancionPrincipal;
+@property (weak, nonatomic) IBOutlet MarqueeLabel *labelAnimado4Canciones;
 
 
 
