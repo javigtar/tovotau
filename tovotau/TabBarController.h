9f86bdc4431b37dec6509fe25703646ba56d9703
alumno <alumno@Mac-mini-de-adminitrador.local>
2015-02-18 09:11:57 +0100

    tabbarcontroller creado

---
+++ tovotau/TabBarController.h
@@ -0,0 +1,13 @@
+//
+//  TabBarController.h
+//  tovotau
+//
+//  Created by alumno on 18/2/15.
+//  Copyright (c) 2015 alumno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabBarController : UITabBarController
+
+@end
